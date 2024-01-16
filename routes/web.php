797cec504d68e8c80d69e2a65b1f3cd50415a2d1
Damaysah <112998606+Damaysah@users.noreply.github.com>
2024-01-16 14:07:48 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// routes/web.php
+
+
+Route::prefix('dosen')->group(function () {
+    // Route untuk halaman profil dosen
+    Route::get('/profil', function () {
+        return view('v_profil', ['message' => 'Halaman profil dosen']);
+    });
+
+    // Route untuk halaman data
+    Route::get('/data', function () {
+        return view('v_data', ['message' => 'Halaman data dosen']);
+    });
+
+    // Route untuk halaman data pengampu
+    Route::get('/data_pengampu', function () {
+        return view('v_ampu', ['message' => 'Halaman Data Mata Kuliah Yang Diampu']);
+});
+});
